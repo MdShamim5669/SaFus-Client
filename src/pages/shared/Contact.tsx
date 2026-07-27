9b7ebfe2f5191abad2b18719342d1a1ba5dd38e1
MdShamim5669 <tamjidulislamsamim@gamil.com>
2026-07-27 07:22:15 +0600

    feat: embed interactive Google Maps showing Daffodil International University location pin

--- src/pages/shared/Contact.tsx
+++ src/pages/shared/Contact.tsx
@@ -143,6 +143,20 @@ export const Contact: React.FC = () => {
               </div>
             </div>
           </div>
+
+          {/* Interactive Live Google Map showing Daffodil International University, Dhaka */}
+          <div className="mt-12 rounded-2xl overflow-hidden border-2 border-[#D1A054]/40 shadow-2xl h-[420px] md:h-[480px]">
+            <iframe
+              title="SaFus Restaurant Live Map - Daffodil International University"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.4285818987483!2d90.31885447605995!3d23.874404784725048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDaffodil%20International%20University!5e0!3m2!1sen!2sbd!4v1700000000000!5m2!1sen!2sbd"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
 
         {/* Section 2: CONTACT FORM */}
